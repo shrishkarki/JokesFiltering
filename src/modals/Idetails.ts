b0ab769ac69f26fe4d72c0b4a7afd645caba28ff
unknown <shrishkarki6669@gmail.com>
2024-06-19 15:20:58 +0545

    toggleUpdate

---
+++ src/modals/Idetails.ts
@@ -0,0 +1,47 @@
+export interface Idetails {
+  error: boolean;
+  amount: number;
+  jokes: (JokesEntity)[] | null;
+}
+  export interface JokesEntity {
+  category: string;
+  type: string;
+  setup: string | null;
+  delivery: string | null;
+  flags: Flags;
+  safe: boolean;
+  id: number;
+  lang: string;
+  joke: string | null ;
+}
+export interface Flags {
+  nsfw: boolean;
+  religious: boolean;
+  political: boolean;
+  racist: boolean;
+  sexist: boolean;
+  explicit: boolean;
+}
+export interface Istate {
+  loading: boolean;
+  jokes: JokesEntity[];
+  errorMsg: string;
+}
+
+export interface Itoggle{
+  [filterName:string]:boolean,
+  
+}
+
+export interface Icategorysearch{
+  [categorySearch:string]:string
+}
+
+
+export interface paramsInterface{
+  currentPage:number,
+  apiParams:string
+}
+
+
+
